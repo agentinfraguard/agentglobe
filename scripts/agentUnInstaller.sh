4d5351895de89d6d418c4729a7460a0452cc60c7
agentinfraguard <agentinfraguard@gmail.com>
2017-06-20 12:38:51 +0530

    Fix bug on kill command

--- scripts/agentUnInstaller.sh
+++ scripts/agentUnInstaller.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+
 uninstall(){
  
 getValue "serviceFile"    
@@ -7,7 +8,7 @@ getValue "removeProcessCmd"
 
 
 # value of serviceFile was saved at the time of agent installation and it may be
-# agent_controller.sh/agent_controller.service/agent_controller_ubuntu.sh
+# agent_controller.sh or agent_controller.service or agent_controller_ubuntu.sh
 echo "serviceFile = : $serviceFile"
 echo "removeProcessCmd = : $removeProcessCmd"
 
@@ -22,36 +23,36 @@ fi
 
 
 echo "Stopping the service..."
-command="/etc/init.d/$serviceFile stop"
-$command
+# command="pkill  $serviceFile"
+# $command
 
-
-pId=$(ps -ef | grep 'infraGuardMain' | grep -v 'grep' | awk '{ printf $2 }')
-echo "Stopping the process i.e infraGuardMain."
-command="/bin/kill -9 $pId"
-$command
+killTheProcess
 
 
 # Restrict service to restart on reboot
 # On the basis of linux type either it has update-rc.d -f ... or chkconfig --del  ....
 # value of removeProcessCmd was saved at the time of agent installation
+echo "Restrict process to restart on reboot..."
 $removeProcessCmd 
 
-echo "Process $pId killed successfully " 
 
 
-echo "Deleting all concerned directories & file..."
+
+echo "Deleting /opt/infraguard/ directory..."
 command="rm -rf /opt/infraguard/"
 $command
 
+
+echo "Deleting /var/logs/infraguard/ directory..."
 command="rm -rf /var/logs/infraguard/"
 $command
 
+echo "Deleting service file at /etc/init.d/$serviceFile..."
 command="rm -rf /etc/init.d/$serviceFile"
 $command
 
 echo ""
-echo "Uninstallation process completes."
+echo "****************** Uninstallation process completes. *******************"
 
 } #Uninstall
 
@@ -78,6 +79,42 @@ getValue(){
       fi
 
   done < "$fileName"
+}
+
+
+killTheProcess(){
+   # Getting the PID of the process
+   PID=$(ps -ef | grep 'infraGuardMain' | grep -v 'grep' | awk '{ printf $2 }')
+   echo "PID = : $PID"
+
+   # Number of seconds to wait before using "kill -9"
+   WAIT_SECONDS=10
+
+   # Counter to keep count of how many seconds have passed
+   count=0
+
+   while kill $PID > /dev/null
+   do
+       # Wait for one second
+       sleep 1
+       # Increment the second counter
+       ((count++))
+
+       # Has the process been killed? If so, exit the loop.
+       if ! ps -p $PID > /dev/null ; then
+           break
+       fi
+
+       # Have we exceeded $WAIT_SECONDS? If so, kill the process with "kill -9"
+       # and exit the loop
+       if [ $count -gt $WAIT_SECONDS ]; then
+           kill -9 $PID
+           break
+       fi
+   done
+   echo "Process has been killed after $count seconds."
+
+
 }
 
 # Check whether user has root level access or not.
